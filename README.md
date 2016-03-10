0f2963566419442e766cca8a45c041304446f793
MockingBird <CodingMonkey.zh@gmail.com>
2016-03-10 18:24:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# remind-me
+
+一个事件清单提醒应用，用 react.js + Redux 进行开发，通过 [electron](https://github.com/atom/electron) 打包成桌面应用。
---
+++ webpack.config.development.js
@@ -0,0 +1,53 @@
+/* eslint strict: 0 */
+'use strict';
+
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpackTargetElectronRenderer = require('webpack-target-electron-renderer');
+const baseConfig = require('./webpack.config.base');
+
+
+const config = Object.create(baseConfig);
+
+const isDevelopment = true;
+
+config.debug = true;
+
+config.devtool = 'cheap-module-eval-source-map';
+
+config.entry = [
+  'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+  './app/index'
+];
+
+config.output.publicPath = 'http://localhost:3000/dist/';
+
+config.module.loaders.push({
+  test: /\.global\.css$/,
+  loaders: [
+    'style-loader',
+    'css-loader?sourceMap'
+  ]
+}, {
+  test: /^((?!\.global).)*\.css$/,
+  loaders: [
+    'style-loader',
+    'css-loader?modules&sourceMap&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+  ]
+});
+
+
+config.plugins.push(
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.NoErrorsPlugin(),
+  new webpack.DefinePlugin({
+    __DEV__: true,
+    'process.env': {
+      NODE_ENV: JSON.stringify('development')
+    }
+  })
+);
+
+config.target = webpackTargetElectronRenderer(config);
+
+module.exports = config;
